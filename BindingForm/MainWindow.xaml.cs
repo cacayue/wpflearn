034d974c314c762ba6f81e8db248ff605fc49ee8
mafutian <a15453606@outlook.com>
2025-07-18 08:12:36 +0800

    数据绑定练习

---
+++ BindingForm/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingForm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Student stu;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            stu = new Student();
+
+            var binding = new Binding()
+            {
+                Source = stu,
+                Path = new PropertyPath("Name"),
+            };
+
+            BindingOperations.SetBinding(this.textBoxName, TextBox.TextProperty, binding);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            stu.Name += " Name";
+        }
+    }
+}
---
+++ BindingForm/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingForm
+{
+    public class Student: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string? name;
+        
+        public string? Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+       
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
